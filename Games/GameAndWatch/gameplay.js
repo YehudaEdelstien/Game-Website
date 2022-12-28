9ef2e318fd74893caf629e1f707f647c145a3f54
Meby123B <meby123b@gmail.com>
2022-12-28 21:44:58 +0200

    Update gameplay.js
    colision
    מזהה רק פינות...

--- Games/GameAndWatch/gameplay.js
+++ Games/GameAndWatch/gameplay.js
@@ -4,18 +4,18 @@ const ctx = canvas.getContext('2d');
 
 const image = new Image()
 image.src = 'https://upload.wikimedia.org/wikipedia/commons/thumb/9/9b/DVD_logo.svg/1200px-DVD_logo.svg.png';
-const imgw = image.width * 0.08;
-const imgh = image.height * 0.08;
+const imgw = image.width * 0.1//0.08;
+const imgh = image.height * 0.1//0.08;
 
 const colorsArr = ['red', 'blue', 'green', 'violet'
 ];
 
 let x = 0;
 let y = 0;
-let playerSize = 50
-let pPos = 20;
-let px = 20;
-let py = canvas.height - 40 - playerSize;
+let playerSize = 500
+let pPos = 40;
+let px = 200//20;
+let py = 150 //canvas.height - 40 - playerSize;
 let valueX = -1;
 let valueY = -1;
 let colorPos = 0;
@@ -25,10 +25,11 @@ let winCounter = 0;
 function main() {
     ctx.clearRect(0, 0, canvas.width, canvas.height)
     makePosition()
+    let colisionColor = getColision(px, py, playerSize, playerSize, x, y, imgw, imgh)
     drawSquare(x, y)
     drawDVD(x, y);
-    drawPlayer(px, py);
-
+    drawPlayer(px, py, colisionColor);
+    
     wincheck(x, y);
 }
 
@@ -80,8 +81,8 @@ function wincheck(x, y) {
     }
 }
 
-function drawPlayer(px, py) {
-    ctx.fillStyle = 'black'
+function drawPlayer(px, py, c) {
+    ctx.fillStyle = c? 'grey' : 'black'
     ctx.fillRect(px, py, playerSize, playerSize)
 }
 
@@ -90,12 +91,24 @@ function movePlayer(num) {
 }
 
 function move() {
-    if (px <= 0 || px >= canvas.width - playerSize - 20) {
+    if (px + pPos < 20 || px + pPos > canvas.width - playerSize) {
         pPos *= -1;
     }
     movePlayer(pPos)
 }
 
+function getColision(x1, y1, w1, h1, x2, y2, w2, h2) {
+    if (
+        x1 > x2     &&  x1 < x2+w2    &&  y1 > y2   &&  y1 < y2+h2   ||
+        x1+w1 > x2  &&  x1+w1 < x2+w2 &&  y1 > y2   &&  y1 < y2+h2   ||
+        x1 > x2     && x1 < x2+w2     && y1+h1 > y2 && y1+h1 < y2+h2 ||
+        x1+w1 > x2  && x1+w1 < x2+w2  && y1+h1 > y2 && y1+h1 < y2+h2
+    ) {
+        console.log('we have colision!!!');
+        return true;
+    }
+}
 
-setInterval(move, 1000 / 2)
+
+// setInterval(move, 1000 / 2)
 setInterval(main, 1000 / 60);
